5eca5f9e5a692ce8be8dc85bec3bad1f70d864fc
MarioSanzRodrigo <m.sanz741@gmail.com>
2018-05-29 14:59:51 +0200

    modify path

--- client_lhe.sh
+++ client_lhe.sh
@@ -1,2 +1,2 @@
-cd /home/mario/Documentos/github/MediaProcessors_selfcontained/gaminganywhere/bin
-LD_LIBRARY_PATH=../../_install_dir_x86/lib ./ga-client ./config/client.mlhe.abs.conf rtsp://138.4.7.149:8554/desktop
+cd <path your gaminganywhere>/bin
+LD_LIBRARY_PATH=../../_install_dir_x86/lib ./ga-client ./config/client.mlhe.abs.conf rtsp://x.x.x.x:8554/desktop
--- server_h264.sh
+++ server_h264.sh
@@ -1,2 +1,2 @@
-cd /home/mario/Documentos/github/MediaProcessors_selfcontained/gaminganywhere/bin
+cd cd <path your gaminganywhere>/bin/bin
 LD_LIBRARY_PATH=../../_install_dir_x86/lib ./ga-server-periodic ./config/server.h264.mame.conf
