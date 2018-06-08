a83e9cdc6422a669b15d269cb3c2c6bb94b11025
mariosanzrodrigo <m.sanz741@gmail.com>
2018-06-08 14:58:45 +0200

    dockerfile OK

--- Dockerfile/server.sh
+++ Dockerfile/server.sh
@@ -1,2 +1,2 @@
-cd /home/admin/gaminganywhere/bin
+cd /home/admin/FFMPEG-Control-API-Arqueopterix/gaminganywhere/bin
 LD_LIBRARY_PATH=/home/admin/FFMPEG-Control-API-Arqueopterix/_install_dir_x86/lib ./ga-server-periodic ./config/server.mlhe.mame.conf
