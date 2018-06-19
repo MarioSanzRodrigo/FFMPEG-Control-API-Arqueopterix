616e0b34769e6f3797e89cbc84bddaddc8077434
MarioSanzRodrigo <m.sanz741@gmail.com>
2018-06-19 11:47:35 +0200

    updated mediaprocessors

--- server_lhe.sh
+++ server_lhe.sh
@@ -1,2 +1,2 @@
-cd /home/mario/0_PROYECTOS_UPM/ARQUEOPTERIX/Arqueo_prueba/gaminganywhere/bin
+cd /home/mario/Documentos/Github_Test/FFMPEG-Control-API-Socket/arqueo_develop_2/gaminganywhere/bin
 LD_LIBRARY_PATH=../../_install_dir_x86/lib ./ga-server-periodic ./config/server.mlhe.mame.conf
