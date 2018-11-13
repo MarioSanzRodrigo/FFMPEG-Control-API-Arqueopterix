8ffd593fc7177df9c990b8841ac5f734ca1e2ac6
MarioSanzRodrigo <m.sanz741@gmail.com>
2018-11-13 11:42:31 +0100

    added resize full screen

--- client_lhe.sh
+++ client_lhe.sh
@@ -1,2 +1,2 @@
 cd gaminganywhere/bin
-LD_LIBRARY_PATH=../../_install_dir_x86/lib ./ga-client ./config/client.mlhe.abs.conf rtsp://localhost:8554/desktop
+LD_LIBRARY_PATH=../../_install_dir_x86/lib ./ga-client ./config/client.mlhe.abs.conf rtsp://138.4.7.248:8554/desktop
